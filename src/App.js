b1fb1a6a7748e68bd7296b0b0dfe58439dd15c3c
Brett Monzel <brett@MWA272.attlocal.net>
2020-09-22 23:32:54 -0700

    initialized alan.ai

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alanKey = '58a43290e335daa62874bb71e6f3febb2e956eca572e1d8b807a3e2338fdd0dc/stage';
+const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles }) =>  {
+                if( command === 'newHeadlines' ){
+                    console.log(articles);
+                }
+            }
+
+        })
+    }, [])
+    return (
+        <div>
+            <h1>Alan AI New Application</h1>
+        </div>
+    );
 }
 
 export default App;
