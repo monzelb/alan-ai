a3c69246e66a492aa43dd28cbd22e6ba708099d1
Brett Monzel <brett@MWA272.attlocal.net>
2020-09-24 22:41:19 -0700

    add material ui elements

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
-
+import NewsCards from './components/NewsCards/NewsCards';
 const alanKey = '58a43290e335daa62874bb71e6f3febb2e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
-
+    const [newsArticles, setNewsArticles] = useState([]);
     useEffect(() => {
         alanBtn({
             key: alanKey,
             onCommand: ({ command, articles }) =>  {
                 if( command === 'newHeadlines' ){
-                    console.log(articles);
+                    setNewsArticles(articles);
                 }
             }
 
@@ -18,6 +18,7 @@ const App = () => {
     return (
         <div>
             <h1>Alan AI New Application</h1>
+            <NewsCards articles={newsArticles} />
         </div>
     );
 }
