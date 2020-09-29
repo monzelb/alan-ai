fc8774d8b70be30fefa02fed20124a9c7edfedc1
Brett Monzel <brett@MWA272.attlocal.net>
2020-09-28 22:42:01 -0700

    add functionality for term and category

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
 const alanKey = '58a43290e335daa62874bb71e6f3febb2e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles();
     useEffect(() => {
         alanBtn({
             key: alanKey,
@@ -17,7 +19,9 @@ const App = () => {
     }, [])
     return (
         <div>
-            <h1>Alan AI New Application</h1>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="Alan Logo" />
+            </div>
             <NewsCards articles={newsArticles} />
         </div>
     );
