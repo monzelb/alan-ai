1585c8d98fdc8aeb230113cc789a1a34a6a4db8c
Brett Monzel <brettmonzel@BRETTs-MBP.attlocal.net>
2022-09-11 23:18:48 -0700

    Quick fix

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -9,7 +9,7 @@ const infoCards = [
     { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
     { color: '#1565c0', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
     { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5' },
-    { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
+    { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from Wired' },
 ];
 
 const NewsCards = ({articles, activeArticle}) => {
@@ -23,7 +23,7 @@ const NewsCards = ({articles, activeArticle}) => {
                         <Grid item xs={12} sm={6} md={4} lg={3} className={classes.infoCard}>
                             <div className={classes.card} style={{backgroundColor: infoCard.color }}>
                                 <Typography variant="h5">{infoCard.title}</Typography>{ infoCard.info ? (<Typography variant="h6"><strong>{infoCard.title.split(' ')[2]}: </strong><br />{infoCard.info}</Typography>) : null }
-                                <Typography variant="h6">Try saying: <br /> <i>{infoCard.text}</i></Typography> 
+                                <Typography variant="h6">Try saying: <br /> <i>{infoCard.text}</i></Typography>
                             </div>
 
                         </Grid>
@@ -45,7 +45,7 @@ const NewsCards = ({articles, activeArticle}) => {
                 ))}
             </Grid>
 
-            
+
         </Grow>
 
 
